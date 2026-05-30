bf733b98e7126d953c5b31c9799cc328cdbad14e
isHardCoded <ishardcoded@gmail.com>
2026-05-30 18:02:40 +0500

    feat: сверстали верхнюю часть профиля

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { ROUTES } from './constants/routes'
 import HomePage from './pages/home'
 import { Layout } from './layout'
+import ProfilePage from './pages/profile'
 
 createRoot(document.getElementById('root')!).render(
   <Router>
     <Routes>
       <Route element={<Layout />}>
         <Route path={ROUTES.HOME} element={<HomePage />} />
+        <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
       </Route>
     </Routes>
   </Router>
---
+++ src/pages/profile.tsx
@@ -0,0 +1,32 @@
+import { ArrowLeft, Ellipsis, Mail } from "lucide-react";
+import { Button } from "../components/ui/button";
+
+export default function ProfilePage() {
+  return <div>
+    <header className="flex items-center py-1 px-4">
+      <Button variant="ghost">
+        <ArrowLeft />
+      </Button>
+      <div>
+        <p className="text-[20px] font-bold leading-[24px]">Stas Neprokin</p>
+        <p className="text-[#6E767D] text-[13px] leading-[16px]">23 post</p>
+      </div>
+    </header>
+    <div className="px-4">
+      <div className="flex items-start justify-between mt-4">
+        <img src="https://mockmind-api.uifaces.co/content/human/80.jpg" className="size-[132px] rounded-full object-cover" alt="" />
+        <div className="flex items-center gap-4">
+          <Button variant="ghost" className="border border-[#6E767D] rounded-full">
+            <Ellipsis size={20} />  
+          </Button>
+          <Button variant="ghost" className="border border-[#6E767D] rounded-full">
+            <Mail size={20} />  
+          </Button>
+          <Button className="rounded-full text-[15px] font-bold leading-[20px] py-2 px-5 text-black bg-white">
+            Follow
+          </Button>
+        </div>
+      </div>
+    </div>
+  </div>
+}
